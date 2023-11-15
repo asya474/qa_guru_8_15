4de0e9f690f4656ddc50865db833ad6beea79440
asya474 <asya474@gmail.com>
2023-11-15 09:41:04 +0700

    add tests

--- tests/test_tinkoff.py
+++ tests/test_tinkoff.py
@@ -1,27 +1,43 @@
 from model.tinkoff_page import TinkoffPage
 from data.users import User
 import allure
+from selene import browser, be, have
 
 tinkoff_page = TinkoffPage()
 
 
-def test_one(setup_browser):
+def test_debet_cart_tinkoff_black_button(setup_browser):
     with allure.step("Открыть главную страницу"):
-        pass
+        browser.element('#cbEZbIs7E').click()
+        browser.should(have.url_containing(
+            f'/cards/debit-cards/tinkoff-black/?internal_source=home_slider_block'
+        ))
 
-def test_two(setup_browser):
+def test_deposits_button(setup_browser):
     with allure.step("Открыть главную страницу"):
-        pass
+        browser.element('#cbn4iE95H').click()
+        browser.should(have.url_containing(
+            f'/savings/deposit/?internal_source=home_icon'
+        ))
 
-def test_three(setup_browser):
+def test_investition_button(setup_browser):
     with allure.step("Открыть главную страницу"):
-        pass
+        browser.element('#cbn4iE95H').click()
+        browser.should(have.url_containing(
+            f'/invest/?internal_source=home_icon'
+        ))
 
-def test_four(setup_browser):
+def test_insurance_button(setup_browser):
     with allure.step("Открыть главную страницу"):
-        pass
+        browser.element('#cbn4iE95H').click()
+        browser.should(have.url_containing(
+            f'/insurance/?internal_source=home_icon'
+        ))
 
-def test_five(setup_browser):
+def test_travel_button(setup_browser):
     with allure.step("Открыть главную страницу"):
-        pass
+        browser.element('#cbn4iE95H').click()
+        browser.should(have.url_containing(
+            f'/travel/?internal_source=home_icon'
+        ))
 
